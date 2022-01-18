bed66da2f92211ba78a16386456650d3143fd6d9
Artem <68074370+SedifBuster@users.noreply.github.com>
2022-01-18 18:06:32 +0300

    Delete text directory

--- text/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("keypress", function (e) {
-    if (e.keyCode === 32) {
-        let element = document.getElementsByClassName( "name" )[0];
-
-        element.parentNode.removeChild(element);
-    }
-})
